8d2bd790ac10157e3245a4e7328a8b9ec2d5e0e5
argonavt11 <argonavt.al@yandex.ru>
2018-09-17 17:45:52 +0300

    add sketch types

--- gosketch/page.go
+++ gosketch/page.go
@@ -164,6 +164,283 @@ type Style struct {
 	TextStyle           TextStyle
 }
 
+type SharedStyle struct {
+	DoObjectID string
+	Name       string
+	Value      Style
+}
+
+type ExportFormat struct {
+	AbsoluteSize     int
+	FileFormat       string
+	Name             string
+	NamingScheme     int
+	Scale            int
+	VisibleScaleType int
+}
+
+type ExportOptions struct {
+	ExportFormats []ExportFormat
+	LayerOptions  int
+	ShouldTrim    bool
+}
+
+type SharedStyleContainer struct {
+	Objects []SharedStyle
+}
+
+type MSJSONFileReference struct {
+	RefClass string
+	Red      string
+}
+
+type MSAttributedString struct {
+	ArchivedAttributedString map[string]string
+}
+
+type CurvePoint struct {
+	DoObjectID   string
+	CornerRadius int
+	СurveFrom    string
+	CurveMode    int
+	CurveTo      string
+	HasCurveFrom bool
+	HasCurveTo   bool
+	Point        string
+}
+
+type RulerData struct {
+	Base int
+}
+
+type Text struct {
+	DoObjectID                        string
+	ExportOptions                     ExportOptions
+	Frame                             Rect
+	IsFlippedVertical                 bool
+	IsFlippedHorizontal               bool
+	IsLocked                          bool
+	IsVisible                         bool
+	LayerListExpandedType             int
+	Name                              string
+	NameIsFixed                       bool
+	OriginalObjectID                  int
+	ResizingType                      int
+	Rotation                          int
+	ShouldBreakMaskChain              bool
+	Style                             Style
+	AttributedString                  MSAttributedString
+	AutomaticallyDrawOnUnderlyingPath bool
+	DontSynchroniseWithSymbol         bool
+	GlyphBounds                       string
+	HeightIsClipped                   bool
+	LineSpacingBehaviour              int
+	TextBehaviour                     int
+}
+
+type ShapeGroup struct {
+	DoObjectID            string
+	ExportOptions         ExportOptions
+	Frame                 Rect
+	IsFlippedVertical     bool
+	IsFlippedHorizontal   bool
+	IsLocked              bool
+	IsVisible             bool
+	LayerListExpandedType int
+	Name                  string
+	NameIsFixed           bool
+	OriginalObjectID      string
+	ResizingType          int
+	Rotation              int
+	ShouldBreakMaskChain  bool
+	Style                 Style
+	HasClickThrough       bool
+	Layers                []interface{}
+	ClippingMaskMode      int
+	HasClippingMask       bool
+	WindingRule           int
+}
+
+type Path struct {
+	IsClosed bool
+	Points   []CurvePoint
+}
+
+type ShapePath struct {
+	DoObjectID            string
+	ExportOptions         ExportOptions
+	Frame                 Rect
+	IsFlippedVertical     bool
+	IsFlippedHorizontal   bool
+	IsLocked              bool
+	IsVisible             bool
+	LayerListExpandedType int
+	Name                  string
+	NameIsFixed           bool
+	ResizingType          int
+	Rotation              int
+	ShouldBreakMaskChain  bool
+	BooleanOperation      int
+	Edited                bool
+	Path                  Path
+}
+
+type Artboard struct {
+	DoObjectID                     string
+	ExportOptions                  ExportOptions
+	Frame                          Rect
+	IsFlippedVertical              bool
+	IsFlippedHorizontal            bool
+	IsLocked                       bool
+	IsVisible                      bool
+	LayerListExpandedType          int
+	Name                           string
+	NameIsFixed                    bool
+	ResizeType                     int
+	Rotation                       int
+	ShouldBreakMaskChain           bool
+	Style                          Style
+	HasClickThrough                bool
+	Layers                         []interface{}
+	BackgroundColor                Color
+	HasBackgroundColor             bool
+	HorizontalRulerData            RulerData
+	IncludeBackgroundColorInExport bool
+	IncludeInCloudUpload           bool
+	VerticalRulerData              RulerData
+}
+
+type Bitmap struct {
+	DoObjectID            string
+	ExportOptions         ExportOptions
+	Frame                 Rect
+	IsFlippedVertical     bool
+	IsFlippedHorizontal   bool
+	IsLocked              bool
+	IsVisible             bool
+	LayerListExpandedType int
+	Name                  string
+	NameIsFixed           bool
+	ResizeType            int
+	Rotation              int
+	ShouldBreakMaskChain  bool
+	Style                 Style
+	ClippingMask          string
+	FillReplacesImage     bool
+	Image                 MSJSONFileReference
+	NineSliceCenterRect   string
+	NineSliceScale        string
+}
+
+type SymbolInstance struct {
+	DoObjectID                     string
+	ExportOptions                  ExportOptions
+	Frame                          Rect
+	IsFlippedVertical              bool
+	IsFlippedHorizontal            bool
+	IsLocked                       bool
+	IsVisible                      bool
+	LayerListExpandedType          int
+	Name                           string
+	NameIsFixed                    bool
+	ResizeType                     int
+	Rotation                       int
+	ShouldBreakMaskChain           bool
+	Style                          Style
+	HorizontalSpacing              int
+	MasterInfluenceEdgeMaxXPadding int
+	MasterInfluenceEdgeMaxYPadding int
+	MasterInfluenceEdgeMinXPadding int
+	MasterInfluenceEdgeMinYPadding int
+	SymbolID                       int
+	VerticalSpacing                int
+}
+
+type Group struct {
+	DoObjectID            string
+	ExportOptions         ExportOptions
+	Frame                 Rect
+	IsFlippedVertical     bool
+	IsFlippedHorizontal   bool
+	IsLocked              bool
+	IsVisible             bool
+	LayerListExpandedType int
+	Name                  string
+	NameIsFixed           bool
+	OriginalObjectID      string
+	ResizeType            int
+	Rotation              int
+	ShouldBreakMaskChain  bool
+	HasClickThrough       bool
+	Layers                []interface{}
+}
+
+type Rectangle struct {
+	DoObjectID                    string
+	ExportOptions                 ExportOptions
+	Frame                         Rect
+	IsFlippedVertical             bool
+	IsFlippedHorizontal           bool
+	IsLocked                      bool
+	IsVisible                     bool
+	LayerListExpandedType         int
+	Name                          string
+	NameIsFixed                   bool
+	ResizeType                    int
+	Rotation                      int
+	ShouldBreakMaskChain          bool
+	BooleanOperation              int
+	Edited                        bool
+	Path                          Path
+	FixedRadius                   int
+	HasConvertedToNewRoundCorners bool
+}
+
+type Oval struct {
+	DoObjectID            string
+	ExportOptions         ExportOptions
+	Frame                 Rect
+	IsFlippedVertical     bool
+	IsFlippedHorizontal   bool
+	IsLocked              bool
+	IsVisible             bool
+	LayerListExpandedType int
+	Name                  string
+	NameIsFixed           bool
+	ResizeType            int
+	Rotation              int
+	ShouldBreakMaskChain  bool
+	BooleanOperation      int
+	Edited                bool
+	Path                  Path
+}
+
+type SymbolMaster struct {
+	BackgroundColor                  Color
+	DoObjectID                       string
+	ExportOptions                    ExportOptions
+	Frame                            Rect
+	HasBackgroundColor               bool
+	HasClickThrough                  bool
+	horizontalRulerData              RulerData
+	IncludeBackgroundColorInExport   bool
+	IncludeBackgroundColorInInstance bool
+	IncludeInCloudUpload             bool
+	IsFlippedHorizontal              bool
+	IsFlippedVertical                bool
+	IsLocked                         bool
+	IsVisible                        bool
+	LayerListExpandedType            int
+	layers                           []interface{}
+	Name                             string
+	NameIsFixed                      bool
+	Rotation                         int
+	ShouldBreakMaskChain             bool
+	Style                            Style
+	SymbolID                         string
+	VerticalRulerData                RulerData
+}
+
 // GetLyersPage get layers elements by page
 func (s *SketchFile) GetLyersPage(page string) []interface{} {
 	return s.Pages[page].Layers
