190ad8dabe4fe950bce5afbde938329327f17cda
argonavt11 <argonavt.al@yandex.ru>
2018-10-15 17:10:23 +0300

    get style artboard

--- gosketch/css.go
+++ gosketch/css.go
@@ -15,31 +15,36 @@ type BlockCss struct {
 	Height          float64
 	Top             float64
 	Left            float64
-	BackgroundColor string
+	BackgroundColor ColorCss
 	BackgroundImage string
 	BorderRadius    float64
 	BorderWidth     float64
-	BorderColor     string
+	BorderColor     ColorCss
 	BorderStyle     string
 	Children        []interface{}
 }
 
 type TextCss struct {
 	FontSize        float64
-	FontColor       string
+	FontColor       ColorCss
 	FontFamily      string
 	FontWeight      float64
 	Width           float64
 	Height          float64
 	Top             float64
 	Left            float64
-	BackgroundColor string
+	BackgroundColor ColorCss
 	BackgroundImage string
 	BorderRadius    float64
 	BorderWidth     float64
-	BorderColor     string
+	BorderColor     ColorCss
 	BorderStyle     string
 	Children        []interface{}
+}
+
+type ColorCss struct {
+	HEX  string
+	RGBA string
 }
 
 func (s *SketchFile) GetCSS(w http.ResponseWriter, r *http.Request) {
@@ -47,12 +52,31 @@ func (s *SketchFile) GetCSS(w http.ResponseWriter, r *http.Request) {
 	for key, page := range s.Pages {
 		blocks := make([]interface{}, 0)
 		newPage := PageCss{ID: key, Css: blocks}
-		getStyle(&page.Layers, &newPage.Css)
+		for _, item := range page.Layers {
+			switch item.(type) {
+			case Artboard:
+				getStyleArtboard(item.(Artboard), &newPage.Css)
+			}
+		}
+		// getStyle(&page.Layers, &newPage.Css)
 		result = append(result, newPage)
 	}
-	json.NewEncoder(w).Encode(&result)
+	json.NewEncoder(w).Encode(s.Pages)
 }
 
-func getStyle(layer *[]interface{}, result *[]interface{}) {
+// func getStyle(layer *[]interface{}, result *[]interface{}) {
 
+// }
+
+func getStyleArtboard(a Artboard, result *[]interface{}) {
+	var newBlock BlockCss
+	newBlock.Width = a.Frame.Width
+	newBlock.Height = a.Frame.Height
+	newBlock.Left = a.Frame.X
+	newBlock.Top = a.Frame.Y
+	newBlock.BackgroundColor = getFormatsColor(a.BackgroundColor)
+}
+
+func getFormatsColor(c Color) ColorCss {
+	return ColorCss{}
 }
