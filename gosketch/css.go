7493d01618184066eacfe116eddbb23c6a87caf9
argonavt11 <argonavt.al@yandex.ru>
2018-10-22 11:08:02 +0300

    css

--- gosketch/css.go
+++ gosketch/css.go
@@ -2,6 +2,7 @@ package gosketch
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -17,11 +18,11 @@ type BlockCss struct {
 	Height          float64
 	Top             float64
 	Left            float64
-	BackgroundColor ColorCss
+	BackgroundColor string
 	BackgroundImage string
 	BorderRadius    float64
 	Border          []string
-	Shadow          []string
+	BoxShadow       string
 	Children        []interface{}
 }
 
@@ -38,7 +39,7 @@ type TextCss struct {
 	BackgroundImage string
 	BorderRadius    float64
 	Border          []string
-	Shadow          []string
+	BoxShadow       string
 	Children        []interface{}
 }
 
@@ -53,10 +54,8 @@ func (s *SketchFile) GetCSS(w http.ResponseWriter, r *http.Request) {
 		blocks := make([]interface{}, 0)
 		newPage := PageCss{ID: key, Css: blocks}
 		for _, item := range page.Layers {
-			switch item.(type) {
-			case Artboard:
-				getStyleArtboard(item.(Artboard), &newPage.Css)
-			}
+			var newBlock BlockCss
+			newBlock.getStyleBlock(&item, &newPage.Css)
 		}
 		// getStyle(&page.Layers, &newPage.Css)
 		result = append(result, newPage)
@@ -64,47 +63,55 @@ func (s *SketchFile) GetCSS(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(s.Pages)
 }
 
-func getStyleArtboard(a Artboard, result *[]interface{}) {
-	var newBlock BlockCss
-	newBlock.Width = a.Frame.Width
-	newBlock.Height = a.Frame.Height
-	newBlock.Left = a.Frame.X
-	newBlock.Top = a.Frame.Y
-	newBlock.BorderRadius = 0
-	newBlock.BackgroundColor = getFormatsColor(a.BackgroundColor)
-	newBlock.Shadow = getShadow(a.Style.Shadows)
-	newBlock.Border = getBorder(a.Style.Borders)
-	fmt.Println(newBlock)
+func (b *BlockCss) getStyle(l *interface{}, result *[]interface{}) {
+	var shadow string
+	b.Width = a.Frame.Width
+	b.Height = a.Frame.Height
+	b.Left = a.Frame.X
+	b.Top = a.Frame.Y
+	b.BorderRadius = 0
+	b.BackgroundColor = a.BackgroundColor.getFormatsColor().RGBA
+	for index, item := range a.Style.Shadows {
+		s, err := item.getShadow()
+		if err == nil {
+			if index > 0 {
+				shadow = shadow + ", "
+			}
+			shadow = shadow + s
+		}
+	}
+	b.BoxShadow = shadow
+	fmt.Println(b)
 }
 
-func getFormatsColor(c Color) ColorCss {
+func (c *Color) getFormatsColor() ColorCss {
 	rgba := "rgba(" + strconv.Itoa(int(c.Red*255)) + ", " + strconv.Itoa(int(c.Green*255)) + ", " + strconv.Itoa(int(c.Blue*255)) + ", " + strconv.FormatFloat(c.Alpha, 'f', 2, 64) + ")"
 	hex := "#" + strconv.FormatInt(int64(c.Red*255), 16) + strconv.FormatInt(int64(c.Green*255), 16) + strconv.FormatInt(int64(c.Blue*255), 16)
 	return ColorCss{RGBA: rgba, HEX: hex}
 }
 
-func getShadow(s []Shadow) []string {
-	var result []string
-	for _, item := range s {
-		if item.IsEnabled == true {
-			x := strconv.Itoa(int(item.OffsetX)) + "px "
-			y := strconv.Itoa(int(item.OffsetY)) + "px "
-			blur := strconv.Itoa(int(item.BlurRadius)) + "px "
-			color := getFormatsColor(item.Color).RGBA
-			result = append(result, x+y+blur+color)
-		}
+func (s *Shadow) getShadow() (string, error) {
+	var result string
+	if s.IsEnabled == true {
+		x := strconv.Itoa(int(s.OffsetX)) + "px "
+		y := strconv.Itoa(int(s.OffsetY)) + "px "
+		blur := strconv.Itoa(int(s.BlurRadius)) + "px "
+		color := s.Color.getFormatsColor().RGBA
+		result = x + y + blur + color
+	} else {
+		return result, errors.New("Disabled shadow")
 	}
-	return result
+	return result, nil
 }
 
-func getBorder(b []Border) []string {
-	var result []string
-	for _, item := range b {
-		if item.IsEnabled == true {
-			t := strconv.Itoa(int(item.Thickness)) + "px "
-			color := getFormatsColor(item.Color).RGBA
-			result = append(result, t+"solid "+color)
-		}
-	}
-	return result
-}
+// func getBorder(b []Border) []string {
+// 	var result []string
+// 	for _, item := range b {
+// 		if item.IsEnabled == true {
+// 			t := strconv.Itoa(int(item.Thickness)) + "px "
+// 			color := getFormatsColor(item.Color).RGBA
+// 			result = append(result, t+"solid "+color)
+// 		}
+// 	}
+// 	return result
+// }
